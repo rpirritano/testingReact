4821159f8a205330e98f0d889b269645eb41c613
Robert Pirritano <rpirritano@gmail.com>
2018-09-07 12:51:02 -0700

    added first tests

---
+++ src/components/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import CommentBox from './CommentBox';
+import CommentList from './CommentList';
+
+
+export default () => {
+  return (
+    <div>
+      <CommentBox />
+      <CommentList />
+    </div>
+  )
+};
+
+
+
+/*
+//Steps:
+1. Create simple component to verify app is working
+2. Do the same with other components
+
+/*
+export default () => {
+  return <div>I'm the app components</div>
+};
+*/
---
+++ src/components/__tests__/App.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from '../App';
+import CommentBox from '../CommentBox';
+import CommentList from '../CommentList';
+
+it ('shows the comment box', () => {
+  const wrapped = shallow(<App />); //can call wrapped, component
+
+  expect(wrapped.find(CommentBox).length).toEqual(1); //find returns an array - should be 1 or 0 since used length
+});
+
+it('shows the comment list', () => {
+  const wrapped = shallow(<App />);
+
+  expect(wrapped.find(CommentList).length).toEqual(1);
+});
