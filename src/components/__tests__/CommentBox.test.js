25bec3e9e032aa6c8480edc015876a258668a535
Robert Pirritano <rpirritano@gmail.com>
2018-09-07 14:32:24 -0700

    completed tests for commentBox

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -17,23 +17,21 @@ it('has a text area and a button', () => {
   expect(wrapped.find('button').length).toEqual(1);
 });
 
-it('has a text area that users can type in', () => {
-  wrapped.find('textarea').simulate('change', {
-    target: { value: 'new comment' } //how get the target.value
-  }); //use HTML event name (change), not React name (onChange)
-  wrapped.update();
+describe('the text area', () => {
+  beforeEach(() => {
+    wrapped.find('textarea').simulate('change', {
+      target: { value: 'new comment' }
+    });
+    wrapped.update();
+  });
 
-  expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+  it('has a text area that users can type in', () => {
+    expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
 });
 
-it('when from is submitted text area gets emptied', () => {
-  wrapped.find('textarea').simulate('change', {
-    target: { value: 'new comment' }
-  });
-  wrapped.update();
-  //if the above test did not exist, then would have to an expect to see if it will be setState
-  //expect(wrapped.find('textarea').prop('value')).toEqual('');
-  wrapped.find('form').simulate('submit');
-  wrapped.update();
-  expect(wrapped.find('textarea').prop('value')).toEqual('');
+  it('when from is submitted text area gets emptied', () => {
+    wrapped.find('form').simulate('submit');
+    wrapped.update();
+    expect(wrapped.find('textarea').prop('value')).toEqual('');
 })
+});
