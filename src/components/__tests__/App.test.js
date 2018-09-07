7d37a758f63d1dab3e972bf156b276bd1dbaff91
Robert Pirritano <rpirritano@gmail.com>
2018-09-07 13:00:30 -0700

    added Jest's beforeEach helper function

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -4,14 +4,18 @@ import App from 'components/App';
 import CommentBox from '../CommentBox';
 import CommentList from '../CommentList';
 
+let wrapped //define here so tests have it available. Use let since will reassign it many times
+
+beforeEach(() => { //helper function from JEST
+  wrapped = shallow(<App />);
+});
+
 it ('shows the comment box', () => {
-  const wrapped = shallow(<App />); //can call wrapped, component
 
   expect(wrapped.find(CommentBox).length).toEqual(1); //find returns an array - should be 1 or 0 since used length
 });
 
 it('shows the comment list', () => {
-  const wrapped = shallow(<App />);
 
   expect(wrapped.find(CommentList).length).toEqual(1);
 });
