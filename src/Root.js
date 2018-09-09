8b36c429e04db5ae0cfaa6e90c13987e918ca87f
Robert Pirritano <rpirritano@gmail.com>
2018-09-09 10:50:45 -0700

    installed axios, redux-promise, moxios; and set up fetch

--- src/Root.js
+++ src/Root.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+inport reduxPromise from 'redux-promise'
 import reducers from 'reducers';
 
 
 //changed since other component tests dont need inital state, only CommentList does
-//added default for initialState to make it compatible with other tests 
+//added default for initialState to make it compatible with other tests
 export default ({ children, initialState = {} }) => {
+  const store = createStore(reducers, initialState, applyMiddleware(reduxPromise));
+
   return(
-    <Provider store={createStore(reducers, initialState)}>
-      {children}
-    </Provider>
+    <Provider store={store}>{children}</Provider>
   )
 }
