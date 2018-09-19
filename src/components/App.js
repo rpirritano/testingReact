70cf5f0ae5be4d8b3f2b4c92a1f233231e69665a
Robert Pirritano <rpirritano@gmail.com>
2018-09-19 14:13:58 -0700

    wired up state for auth

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,50 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
+import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 import CommentBox from 'components/CommentBox';
 import CommentList from 'components/CommentList';
+import * as actions from 'actions';
 
+class App extends Component {
+  renderButton() {
+    if (this.props.auth) {
+      return (
+        <button>Sign Out</button>
+      );
+    } else {
+      return (
+        <button>Sign In</button>
+      );
+    }
+  }
 
-export default () => {
-  return (
-    <div>
-      <Route path="/post" component={CommentBox} />
-      <Route path="/" exact component={CommentList} />
-    </div>
-  )
-};
+  renderHeader() {
+    return (
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/post">Post A Comment</Link>
+        </li>
+        <li>{this.renderButton()}</li>
+      </ul>
+    );
+  }
 
+  render() {
+    return (
+      <div>
+        {this.renderHeader()}
+        <Route path="/post" component={CommentBox} />
+        <Route path="/" exact component={CommentList} />
+      </div>
+    );
+  }
+}
 
+function mapStateToProps(state) {
+  return { auth: state.auth };
+}
 
-/*
-//Steps:
-1. Create simple component to verify app is working
-2. Do the same with other components
-
-/*
-export default () => {
-  return <div>I'm the app components</div>
-};
-*/
+export default connect(mapStateToProps, actions)(App);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import commentsReducer from 'reducers/comments';
+import authReducer from 'reducers/auth';
 
 export default combineReducers({
-  comments: commentsReducer //comments is the key
+  comments: commentsReducer, //comments is the key
+  auth: authReducer
 });
